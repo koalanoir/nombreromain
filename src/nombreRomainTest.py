423f9c6945477ae7acf87eac812e7e2f62502319
koalanoir <ngdorian0@gmail.com>
2023-01-25 10:26:19 +0100

    test 8

--- src/nombreRomainTest.py
+++ src/nombreRomainTest.py
@@ -29,5 +29,50 @@ class NombreRomainTest(unittest.TestCase):
         # On a I
         self.assertEqual(romain , 'III')
 
+    def test4(self):
+        #Etant donné le chiffre 4
+        nombre=4
+        #quand on le converti en chiffre Romain
+        romain=NombreRomain.convert(nombre)
+
+        # On a I
+        self.assertEqual(romain , 'IV')  
+
+    def test5(self):
+        #Etant donné le chiffre 5
+        nombre=5
+        #quand on le converti en chiffre Romain
+        romain=NombreRomain.convert(nombre)
+
+        # On a I
+        self.assertEqual(romain , 'V')
+
+    def test6(self):
+        #Etant donné le chiffre 6
+        nombre=6
+        #quand on le converti en chiffre Romain
+        romain=NombreRomain.convert(nombre)
+
+        # On a I
+        self.assertEqual(romain , 'VI')
+
+    def test7(self):
+        #Etant donné le chiffre 7
+        nombre=7
+        #quand on le converti en chiffre Romain
+        romain=NombreRomain.convert(nombre)
+
+        # On a I
+        self.assertEqual(romain , 'VII')
+    
+    def test8(self):
+        #Etant donné le chiffre 8
+        nombre=8
+        #quand on le converti en chiffre Romain
+        romain=NombreRomain.convert(nombre)
+
+        # On a I
+        self.assertEqual(romain , 'VIII')
+        
 if __name__ == '__main__':
     unittest.main()
