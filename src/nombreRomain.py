f57fa805a1c0149d5d2ae7273272fea52187cc32
koalanoir <ngdorian0@gmail.com>
2023-01-25 10:23:52 +0100

    test 3

--- src/nombreRomain.py
+++ src/nombreRomain.py
@@ -1,5 +1,9 @@
 class NombreRomain:
     @staticmethod
     def convert(nombre):
-        if(nombre==1):
-            return 'I'
+        listConvertion=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII']
+        i=1
+        while i<=12:
+            if(nombre==i):
+                return listConvertion[i-1]
+            i+=1
--- src/nombreRomainTest.py
+++ src/nombreRomainTest.py
@@ -11,5 +11,23 @@ class NombreRomainTest(unittest.TestCase):
         # On a I
         self.assertEqual(romain , 'I')
 
+    def test2(self):
+        #Etant donné le chiffre 2
+        nombre=2
+        #quand on le converti en chiffre Romain
+        romain=NombreRomain.convert(nombre)
+
+        # On a I
+        self.assertEqual(romain , 'II')
+
+    def test3(self):
+        #Etant donné le chiffre 3
+        nombre=3
+        #quand on le converti en chiffre Romain
+        romain=NombreRomain.convert(nombre)
+
+        # On a I
+        self.assertEqual(romain , 'III')
+
 if __name__ == '__main__':
     unittest.main()
