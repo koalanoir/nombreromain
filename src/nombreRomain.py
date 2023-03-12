a8c2ed4b3ed817e40b07cb74b0143e7e43bdae8e
koalanoir <ngdorian0@gmail.com>
2023-03-12 20:11:41 +0100

    test fast forward test 14

--- src/nombreRomain.py
+++ src/nombreRomain.py
@@ -1,9 +1,9 @@
 class NombreRomain:
     @staticmethod
     def convert(nombre):
-        listConvertion=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII']
+        listConvertion=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV']
         i=1
-        while i<=13:
+        while i<=14:
             if(nombre==i):
                 return listConvertion[i-1]
             i+=1
