15932d75982fb2a9aed25ddb323138bf286fcfbb
koalanoir <ngdorian0@gmail.com>
2023-03-13 00:17:55 +0100

    release 23 correct error

--- src/main/nombreRomain.py
+++ src/main/nombreRomain.py
@@ -3,7 +3,7 @@ class NombreRomain:
     def convert(nombre):
         listConvertion=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX','XXI','XXII','XXIII']
         i=1
-        while i<=22:
+        while i<=23:
             if(nombre==i):
                 return listConvertion[i-1]
             i+=1
